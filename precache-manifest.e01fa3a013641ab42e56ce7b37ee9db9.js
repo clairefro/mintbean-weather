864d41240418fbbf816b67bac24572c1091d21b6
claire froelich <claire.froelich@gmail.com>
2020-06-24 12:16:09 -0400

    Updates

---
+++ precache-manifest.e01fa3a013641ab42e56ce7b37ee9db9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f61ac8f69f2550753b78187fb095107b",
+    "url": "/mintbean-weather/index.html"
+  },
+  {
+    "revision": "a02a5d0eac7c2d3dabbe",
+    "url": "/mintbean-weather/static/css/main.75df9f48.chunk.css"
+  },
+  {
+    "revision": "1cbb7c05faf3f3cf0b3b",
+    "url": "/mintbean-weather/static/js/2.f118ac5e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mintbean-weather/static/js/2.f118ac5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a02a5d0eac7c2d3dabbe",
+    "url": "/mintbean-weather/static/js/main.fca8e2bb.chunk.js"
+  },
+  {
+    "revision": "a828fb7502ee34949c46",
+    "url": "/mintbean-weather/static/js/runtime-main.a0963937.js"
+  },
+  {
+    "revision": "cf18b6297c3065e52d11afe04a8b6110",
+    "url": "/mintbean-weather/static/media/mintbean.cf18b629.png"
+  }
+]);
