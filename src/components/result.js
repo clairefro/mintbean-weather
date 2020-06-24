f99b31f716375f2d7e2c9e432242a35a4d410473
claire froelich <claire.froelich@gmail.com>
2020-06-24 15:53:27 -0400

    add temp small talk

--- src/components/result.js
+++ src/components/result.js
@@ -1,8 +1,37 @@
 import React, { useState, useEffect } from 'react'
 
+import {smalltalk} from '../static/smalltalk'
 const iconEndpoint = 'https://www.weatherbit.io/static/img/icons/'
 
 const Result = ({ data }) => {
+  const [smalltalks, setSmalltalks] = useState(['s'])
+
+  const getSmalltalk = (d) => {
+    let st = []
+
+    // get temp based smalltalk
+    if(d.temp < 18) {
+      st.push(getRandFromArray(smalltalk.temp.low))
+    } else if ( d.temp >= 18 && d.temp <= 29) {
+      st.push(getRandFromArray(smalltalk.temp.med))
+    } else {
+      st.push(getRandFromArray(smalltalk.temp.high))
+    }
+    console.log(st)
+    // get weather based smalltalk
+
+    return st
+
+  }
+
+  const getRandFromArray = (arr) => {
+  return arr[Math.floor(Math.random() * arr.length)];
+  }
+  useEffect(()=> {
+    if (data.weather) {
+      setSmalltalks(getSmalltalk(data))
+    }
+  }, [data])
   return data.weather ? (
     <div className="result-container">
       <h2>Weather in {data.city_name}, {data.country_code}</h2>
@@ -12,6 +41,11 @@ const Result = ({ data }) => {
       </div>
       <p>{data.weather.description}</p>
       <h2>{randomPercent()} chance of hearing this from strangers:</h2>
+      <ul>
+        {smalltalks.map((s,i)=> (
+          <li key={i}>"{s}"</li>
+        ))}
+      </ul>
     </div>
   ) : null
 
